a262769eac3e3b51b4a5e340d0c06b89dc5d046c
FINE-V3rm <56572360+FINE-V3rm@users.noreply.github.com>
2021-06-24 16:35:06 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -15,7 +15,7 @@ module.exports = (bot) => {
             return message.channel.send(`has a HUGE fucking penis`)
         }
         if (message.content === `vyan`) {
-            let porn = ["https://cdn.discordapp.com/attachments/652566036356399148/670475120745250828/7days-1.gif", "https://tenor.com/view/cope-cope-harder-owned-bussin-cheese-burger-gif-21446979", "Vyan is a retard who no one likes."]
+            let porn = ["https://cdn.discordapp.com/attachments/652566036356399148/670475120745250828/7days-1.gif", "https://tenor.com/view/cope-cope-harder-owned-bussin-cheese-burger-gif-21446979", "Vyan is a retard who no one likes.", "https://cdn.discordapp.com/attachments/855230444676186123/855577911528652821/funke.mp4"]
             let rporn = Math.floor(Math.random() * porn.length);
             message.author.send(porn[rporn])
         }
