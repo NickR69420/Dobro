dfaaa888d3afd298e2181e9bc074e0e10ef912be
daysling <daysling@jeekie.host>
2021-08-12 20:01:53 +0545

    Created a proper launcher and a proper updater method

---
+++ launcher.js
@@ -0,0 +1,12 @@
+// Import exec from child process
+var exec = require('child_process').exec;
+
+function rerun() {
+    // Executes 'npm run main:start' and gets its exit code
+    var exitCode = exec('npm run main:start');
+    if(exitCode.exitCode === 2) {
+        rerun();
+    }
+}
+
+rerun()
