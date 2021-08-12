7dcc7d34f459b7fcb18a074c8c3f546e723e03ee
daysling <daysling@jeekie.host>
2021-08-12 19:32:25 +0545

    Added a production and classic check.

--- autoupdater.js
+++ autoupdater.js
@@ -3,10 +3,11 @@ let versionUri = "http://www.example.com/autoupdater.json";
 
 // Get the current version of the app through package.json and split it into major minor and revision numbers.
 let currentVersion = JSON.parse(fs.readFileSync(__dirname + "/package.json", "utf8")).version.split(".");
-let major = currentVersion[0];
-let minor = currentVersion[1];
-let revision = currentVersion[2];
-
+let major = parseInt(currentVersion[0]);
+let minor = parseInt(currentVersion[1]);
+let revision = parseInt(currentVersion[2]);
+// If application is running on production mode it'll be 1 hour otherwise 5 min.
+let initalCheckDelay = process.env.NODE_ENV == "production" ? 3600000 : 300000;
 // Creates a async method that calls a callback after fetching data from versionUri.
 async function getVersionData(callback) {
     let versionData = await fetch(versionUri);
@@ -15,4 +16,31 @@ async function getVersionData(callback) {
     let newVersion = version.version.split(".");
     // Create a callback with array of new Version
     callback(newVersion);
+}
+
+async function checkForUpdate() {
+    // Call getVersionData and pass it a callback.
+    await getVersionData(function(newVersion) {
+        // Compare the current version with the new version.
+        if (newVersion[0] > major) {
+            console.log(`Auto updating to a new major. - ${newVersion.join(".")}`);
+        } else if (newVersion[0] == major && newVersion[1] > minor) {
+            console.log(`Auto updating to a new minor version. - ${newVersion.join(".")}`);
+        } else if (newVersion[0] == major && newVersion[1] == minor && newVersion[2] > revision + 10) {
+            console.log(`Auto updating to a new revision. - ${newVersion.join(".")}`);
+        } else {
+            
+        }
+    });
+
+    setInterval(checkForUpdate, initalCheckDelay);
+}
+
+// Check if the application is running on developement mode.
+if (process.env.NODE_ENV === "development") {
+    // Check for update every 5 minutes.
+    setInterval(checkForUpdate, initalCheckDelay);
+}else {
+    // Check for update every 1 hour.
+    setInterval(checkForUpdate, initalCheckDelay);
 }
